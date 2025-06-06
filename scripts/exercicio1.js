6f84ab6d781d12954434263be9e581c62921ebde
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-06-06 10:46:59 -0300

    Move o último item para o topo

--- scripts/exercicio1.js
+++ scripts/exercicio1.js
@@ -80,8 +80,8 @@ console.log('----------------------');
 
 // Um item da Alpha precisa aparecer no topo da lista para ser destacado na vitrine
 
-inventarioalpha.pop() 
+inventarioAlpha.pop() 
 inventarioAlpha.unshift(`Terminal de Dobra", "Alpha Centauri", 10500`)
-console.log(`A lista com o item alfa no topo ficou: ${inventarioBeta}`);
+console.log(`A lista com o item alfa no topo ficou: ${inventarioAlpha}`);
 
 
